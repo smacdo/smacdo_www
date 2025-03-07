90cd130a2c1516340308b4359b578d72dc9d39c9
Scott MacDonald <scott@smacdo.com>
2025-03-06 21:32:01 -0800

    initial bootstrap import and demo

---
+++ NOTES.md
@@ -0,0 +1,3 @@
+# TODO
+
+- Set light/dark mode based on OS settings: https://getbootstrap.com/docs/5.3/customize/color-modes/#javascript
--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,47 @@
-import { useState } from 'react'
-import { DiGithubBadge  } from 'react-icons/di';
+import {useState} from 'react'
+import {DiGithubBadge} from 'react-icons/di'
+import {Button} from 'react-bootstrap'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [showGame, setShowGame] = useState<boolean>(false)
 
-  return (
-    <>
-        <div>
-            <h1>smacdo.com</h1>
-            <p>[stuff...]</p>
+    return (
+        <>
+            <div className="main-content" id="home">
+                <h1>smacdo.com</h1>
+                <p>[stuff...]</p>
+
+                <div className="card">
+                    <Button variant="secondary" className="mr-1" onClick={() => setShowGame(!showGame)}>
+                        Play a game!
+                    </Button>
+                </div>
+            </div>
+
+            {showGame ? <TicTacToeGame/> : null}
+
+            <Footer/>
+        </>
+    )
+}
+
+function TicTacToeGame() {
+    return (
+        <div className="main-content" id="TicTacToeGame">
+            <h4 className="modal-title">TIC TAC TOE!</h4>
         </div>
+    )
+}
 
-        <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-        </div>
-
+function Footer() {
+    return (
         <div className="footer">
-            <DiGithubBadge /> <a href="https://github.com/smacdo/">smacdo</a>
+            <DiGithubBadge/> <a href="https://github.com/smacdo/">smacdo</a>
         </div>
-    </>
-  )
+    )
 }
 
 export default App
