a853c9278d7a9560fd1070744be833b82d6092ce
Scott MacDonald <scott@smacdo.com>
2025-03-11 20:36:40 -0700

    skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,57 @@
-import {useState} from 'react'
 import {DiGithubBadge} from 'react-icons/di'
-import {Button} from 'react-bootstrap'
+import {MdOutlineLightMode} from "react-icons/md";
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 import './App.css'
 
 function App() {
-    const [showGame, setShowGame] = useState<boolean>(false)
-
     return (
-        <>
+        <div className="col-lg-8 mx-auto p-4">
             <Header/>
-            <div className="container">
-                <p>[stuff...]</p>
-            </div>
 
-            <div className="container">
-                <Button variant="secondary" className="mr-1" onClick={() => setShowGame(!showGame)}>
-                    Play a game!
-                </Button>
-            </div>
-
-            {showGame ? <TicTacToeGame/> : null}
+            <main role="main">
+                <p>[stuff goes here...]</p>
+            </main>
 
             <Footer/>
-        </>
-    )
-}
-
-function TicTacToeGame() {
-    return (
-        <div className="container">
-            <h4 className="modal-title">TIC TAC TOE!</h4>
         </div>
     )
 }
 
 function Header() {
     return (
-        <div className="container">
-            <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-                <a href="/"
-                   className="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-                    <span className="fs-4">smacdo.com</span>
-                </a>
-                <ul className="nav nav-pills">
-                    <li className="nav-item">
-                        <a href="#" className="nav-link active" aria-current="page">Home</a>
+        <header className="masthead" role="banner">
+            <nav className="navbar" role="navigation">
+                <div id="logo">
+                    <a href="/">smacdo.com</a>
+                </div>
+
+                <ul id="menu">
+                    <li><a href="/">Home</a></li>
+                    <li><span>About</span></li>
+                    <li>Contact</li>
+                    <li>Projects</li>
+                    <li>
+                        <MdOutlineLightMode/>
                     </li>
                 </ul>
-
-            </header>
-        </div>
+            </nav>
+        </header>
     )
 }
 
 function Footer() {
     return (
-        <div className="container">
-            <div className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-                <div className="col-md-4 d-flex align-items-center">(C) Scott MacDonald</div>
-                <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-                    <li className="ms-3"><DiGithubBadge/></li>
-                </ul>
+        <footer className="main-footer" role="contentinfo">
+            <div id="badges">
+                <a href="https://github.com/smacdo" target="_blank" className="badgelink">
+                    <DiGithubBadge size={32}/>
+                </a>
             </div>
-        </div>
-
+            <div>
+                Copyright &copy; 2025 Scott MacDonald
+            </div>
+        </footer>
     )
 }
 
