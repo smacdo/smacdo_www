a8e16150c6becb8095fde0d97659399a47f3f83a
Scott MacDonald <scott@smacdo.com>
2025-03-18 20:59:48 -0700

    remove extraneous state and resize at least once flags

--- src/hooks/UseCanvas.ts
+++ src/hooks/UseCanvas.ts
@@ -1,24 +1,20 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useRef} from "react";
 import {not_null} from "../utils";
 
 export type DrawFn = (ctx: CanvasRenderingContext2D, nowTime: DOMHighResTimeStamp, deltaTime: number) => void;
 
 export function useCanvas(onDraw: DrawFn) {
     const canvasRef = useRef<HTMLCanvasElement>(null);
-    const [context, setContext] = useState<CanvasRenderingContext2D | null>(null);
 
     useEffect(() => {
         // Request a 2d canvas context from the HTML canvas element.
         const canvas = not_null(canvasRef.current);
         const context = not_null(canvas.getContext('2d'), "canvas element does not support 2d mode or the mode was already set");
 
-        setContext(context);
-
         // Create a render lambda function that is invoked on regular cadence by the browser,
         // and each time it is called will invoke the user specified `onRender` callback.
         let lastFrameTime = performance.now();
         let lastAnimationFrameId: number | null = null;
-        let resizeFirstTime = true;
 
         const render = (timestamp: DOMHighResTimeStamp) => {
             // Calculate the amount of time that has elapsed since the last draw call.
@@ -28,14 +24,13 @@ export function useCanvas(onDraw: DrawFn) {
             // Handle window resizing.
             const {width, height} = canvas.getBoundingClientRect();
 
-            if (resizeFirstTime || canvas.width !== width || canvas.height !== height) {
+            if (canvas.width !== width || canvas.height !== height) {
                 const {devicePixelRatio: ratio = 1} = window;
 
                 canvas.width = width * ratio;
                 canvas.height = height * ratio;
 
                 context.scale(ratio, ratio);
-                resizeFirstTime = false;
             }
 
             // Invoke the `onDraw` callback to let the user control what is drawn to the screen.
@@ -53,7 +48,7 @@ export function useCanvas(onDraw: DrawFn) {
                 window.cancelAnimationFrame(lastAnimationFrameId);
             }
         }
-    }, [context, onDraw]);
+    }, [onDraw]);
 
     return canvasRef;
 }
