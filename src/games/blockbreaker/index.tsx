8496a844402848a69e973579a787e0f8c4509261
Scott MacDonald <scott@smacdo.com>
2025-03-30 19:59:20 -0700

    paddle

--- src/games/blockbreaker/index.tsx
+++ src/games/blockbreaker/index.tsx
@@ -2,6 +2,11 @@ import Canvas from "../../components/Canvas";
 import {useRef} from "react";
 import {not_null} from "../../utils.tsx";
 
+const PADDLE_WIDTH = 100.0;
+const PADDLE_HEIGHT = 20.0;
+
+const PADDLE_COLOR = "#B0CAB0";
+
 interface BlockDefinition {
     color: string;
 }
@@ -23,13 +28,13 @@ const BLOCKS: BlockDefinition[] = [
 
 interface GameObject {
     /// left x position.
-    pos_x: number;
+    left: number;
     /// top y position.
-    pos_y: number;
-    /// object width.
-    width: number;
-    /// object height.
-    height: number;
+    top: number;
+    /// right x position.
+    right: number;
+    /// bottom y position.
+    bottom: number;
     /// object movement velocity in the x direction (0 for none).
     vel_x: number;
     /// object movement velocity in the y direction (0 for none).
@@ -37,22 +42,40 @@ interface GameObject {
 }
 
 class Block implements GameObject {
-    pos_x: number;
-    pos_y: number;
-    width: number;
-    height: number;
+    left: number;
+    top: number;
+    right: number;
+    bottom: number;
     vel_x: number;
     vel_y: number;
     def: BlockDefinition;
 
-    constructor(pos_x: number, pos_y: number, width: number, height: number, def: BlockDefinition) {
-        this.pos_x = pos_x;
-        this.pos_y = pos_y;
-        this.width = width;
-        this.height = height;
+    constructor(left: number, top: number, width: number, height: number, def: BlockDefinition) {
+        this.left = left;
+        this.top = top;
+        this.right = left + width;
+        this.bottom = top + height;
         this.vel_x = 0;
         this.vel_y = 0;
         this.def = def;
+    }
+}
+
+class Paddle implements GameObject {
+    left: number;
+    top: number;
+    right: number;
+    bottom: number;
+    vel_x: number;
+    vel_y: number;
+
+    constructor(center_x: number, center_y: number, width: number, height: number) {
+        this.left = center_x - width / 2;
+        this.top = center_y - height / 2;
+        this.right = center_x + width / 2;
+        this.bottom = center_y + height / 2;
+        this.vel_x = 0;
+        this.vel_y = 0;
     }
 }
 
@@ -61,9 +84,11 @@ class Block implements GameObject {
 //       Alternatively, store the height (maybe width) as a percentage in the level.
 class GameLevel {
     blocks: Block[];
+    paddles: Paddle[];
 
     constructor(levelWidth: number, levelHeight: number, blocks: number[][]) {
         this.blocks = [];
+        this.paddles = [new Paddle(levelWidth / 2, levelHeight - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT)];
         this.load(levelWidth, levelHeight, blocks);
     }
 
@@ -78,7 +103,7 @@ class GameLevel {
 
         // Generate game objects from the provided block map.
         const blockWidth = colCount > 0 ? levelWidth / colCount : 0;
-        const blockHeight = rowCount > 0 ? levelHeight / rowCount : 0;
+        const blockHeight = rowCount > 0 ? levelHeight / 2 / rowCount : 0;
 
         for (let row = 0; row < rowCount; row++) {
             for (let col = 0; col < initialBlocks[row].length; col++) {
@@ -123,28 +148,41 @@ class BlockBreakerGame {
             [0, 0, 0, 0, 0, 0],
         ];
 
-        this.currentLevel = new GameLevel(canvasWidth, canvasHeight / 2, blocks);
+        this.currentLevel = new GameLevel(canvasWidth, canvasHeight, blocks);
     }
 
     public onDraw(ctx: CanvasRenderingContext2D, _nowTime: number, _deltaTime: number) {
-        // TODO: Optimize rendering by drawing background and blocks to an offscreen canvas.
-
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
         // Draw the background.
         // TODO: Find a fancier background than just black or white.
+        // TODO: Optimize rendering by drawing background to an offscreen canvas.
         ctx.fillStyle = '#F0F0F0';
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
-        // Draw the blocks.
-        // TODO: Draw the blocks to an offscreen canvas that only updates as needed.
+        // Draw the game level (blocks, paddles, balls etc).
         if (this.currentLevel) {
-            for (let blockIndex = 0; blockIndex < this.currentLevel.blocks.length; blockIndex++) {
-                const block = this.currentLevel.blocks[blockIndex];
+            this.drawBlocks(ctx, this.currentLevel);
+            this.drawPaddles(ctx, this.currentLevel);
+        }
+    }
 
-                ctx.fillStyle = block.def.color;
-                ctx.fillRect(block.pos_x, block.pos_y, block.width, block.height);
-            }
+    drawBlocks(ctx: CanvasRenderingContext2D, level: GameLevel) {
+        // TODO: Draw the blocks to an offscreen canvas that only updates as needed.
+        for (let blockIndex = 0; blockIndex < level.blocks.length; blockIndex++) {
+            const block = level.blocks[blockIndex];
+
+            ctx.fillStyle = block.def.color;
+            ctx.fillRect(block.left, block.top, block.right - block.left, block.bottom - block.top);
+        }
+    }
+
+    drawPaddles(ctx: CanvasRenderingContext2D, level: GameLevel) {
+        for (let paddleIndex = 0; paddleIndex < level.paddles.length; paddleIndex++) {
+            const paddle = level.paddles[paddleIndex];
+
+            ctx.fillStyle = PADDLE_COLOR;
+            ctx.fillRect(paddle.left, paddle.top, paddle.right - paddle.left, paddle.bottom - paddle.top);
         }
     }
 }
