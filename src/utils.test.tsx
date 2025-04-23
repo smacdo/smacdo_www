d169c8748ebbe52f273d3d285d05731a93655d8c
Scott MacDonald <scott@smacdo.com>
2025-04-22 17:39:25 -0700

    add comments and tests to utils module

--- src/utils.test.tsx
+++ src/utils.test.tsx
@@ -1,7 +1,32 @@
 import '@testing-library/jest-dom';
-import {not_null} from "./utils";
+import {clamp, not_null} from "./utils";
 
-describe("not_null", () => {
+describe('clamp', () => {
+    test('returns the number if it is between min and max', () => {
+        expect(clamp(3, 3, 7)).toBe(3);
+        expect(clamp(4, 3, 7)).toBe(4);
+        expect(clamp(5, 3, 7)).toBe(5);
+        expect(clamp(6, 3, 7)).toBe(6);
+        expect(clamp(7, 3, 7)).toBe(7);
+    });
+
+    test('returns min if the value is less than min', () => {
+        expect(clamp(2, 3, 7)).toBe(3);
+        expect(clamp(1, 3, 7)).toBe(3);
+        expect(clamp(0, 3, 7)).toBe(3);
+        expect(clamp(-1, 3, 7)).toBe(3);
+        expect(clamp(-100, 3, 7)).toBe(3);
+    });
+
+    test('returns max if the value is greater than max', () => {
+        expect(clamp(8, 3, 7)).toBe(7);
+        expect(clamp(9, 3, 7)).toBe(7);
+        expect(clamp(10, 3, 7)).toBe(7);
+        expect(clamp(100, 3, 7)).toBe(7);
+    });
+});
+
+describe('not_null', () => {
     test('returns the value if it is not null', () => {
         expect(not_null(123)).toEqual(123);
         expect(not_null("hello")).toEqual("hello");
@@ -16,5 +41,9 @@ describe("not_null", () => {
 
     test('throws an error if the value is null', () => {
         expect(() => not_null(null)).toThrow(/cannot be null or undefined/);
-    })
+    });
+
+    test('throws an error with a custom message', () => {
+        expect(() => not_null(null, "custom message")).toThrow(/custom message/);
+    });
 });
--- src/utils.tsx
+++ src/utils.tsx
@@ -1,7 +1,18 @@
+/**
+ * Returns the value `v` clamped to the range [`min`, `max`].
+ * @param v The value to clamp.
+ * @param min The minimum value that `v` can be.
+ * @param max The maximum value that `v` can be.
+ */
 export function clamp(v: number, min: number, max: number) {
     return Math.min(Math.max(v, min), max);
 }
 
+/**
+ * Returns the passed value if it is both defined and not null, otherwise throws an exception.
+ * @param value A value which should be defined and not null.
+ * @param message An optional exception message when the value is undefined or null.
+ */
 export function not_null<T>(value: T | null | undefined, message = "Value cannot be null or undefined"): T {
     if (value === null || value === undefined) {
         throw new Error(message);
