6ceced9e7eea5abceb7d7c565d31d4c6c956b824
Scott MacDonald <scott@smacdo.com>
2025-07-06 16:01:51 -0700

    remove PageLink test that used browser events for now

--- src/components/PageLink.test.tsx
+++ src/components/PageLink.test.tsx
@@ -1,5 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import {beforeEach, describe, expect, vi} from 'vitest';
+
 import '@testing-library/jest-dom';
 
 import PageLink from './PageLink';
@@ -35,38 +36,4 @@ describe('PageLink', () => {
         // Assert that the anchor's href attribute is correct.
         expect(linkElement).toHaveAttribute('href', testPath);
     });
-
-    /* TODO - this doesn't seem to fire the event correctly. Need to investigate.
-
-    it('should prevent default navigation behavior, update history and notify event listeners of a url change', () => {
-        window.history.replaceState = vi.fn();
-
-        render(<PageLink path={testPath}>{testChildren}</PageLink>);
-        const linkElement = screen.getByText(testChildren);
-        expect(linkElement).toBeInTheDocument();
-
-        // Simulate a click event on the link without any modifier keys.
-        const mockClickEvent = {
-            preventDefault: vi.fn(), // mock this to check that it is called.
-            ctrlKey: false,
-            metaKey: false,
-            shiftKey: false
-        } as unknown as React.MouseEvent<HTMLAnchorElement, MouseEvent>;
-
-        fireEvent.click(linkElement, mockClickEvent);
-
-        // Verify that preventDefault was called, which stops the browser's default behavior of
-        // reloading the page.
-        expect(mockClickEvent.preventDefault).toHaveBeenCalledTimes(1);
-
-        // Check that the requested link was added to the browser window's history via pushState.
-        expect(window.history.replaceState).toHaveBeenCalledTimes(0);
-        expect(window.history.pushState).toHaveBeenCalledTimes(1);
-        expect(window.history.pushState).toHaveBeenCalledWith({}, '', testPath);
-
-        // Verify any active event listeners have been notified that the URL changed.
-        expect(window.dispatchEvent).toHaveBeenCalledTimes(1);
-        expect(window.dispatchEvent).toHaveBeenCalledWith(new PopStateEvent('popstate'));
-    });
-     */
 });
