d6212bc07bcd3ebce33cd75339f6047ac66a2c51
Scott MacDonald <scott@smacdo.com>
2026-09-13 09:57:03 -0700

    render test level

--- src/demos/brainfreeze/src/game.js
+++ src/demos/brainfreeze/src/game.js
@@ -1,3 +1,4 @@
+import level from "./levels/level1.js";
 export class Game {
   /**
    * @param {CanvasRenderingContext2D} canvasContext
@@ -75,12 +76,114 @@ export class Game {
    * Draw game state.
    */
   render() {
-    const canvas = this.canvasContext.canvas;
+    //const canvas = this.canvasContext.canvas;
 
+    // Style configuration.
+    const TILE_WALL = 0;
+
+    const WALL_COLOR = "white";
+    const FLOOR_COLOR = "gray";
+    const PLAYER_COLOR = "yellow";
+    const BOX_COLOR = "brown";
+    const GOAL_EMPTY_COLOR = "red";
+    const GOAL_FULL_COLOR = "pink";
+
+    // Tile map configuration.
+    const tileWidth = 64;
+    const tileHeight = 64;
+
+    const drawTileBorders = true;
+
+    if (drawTileBorders) {
+      this.canvasContext.strokeStyle = "black";
+      this.canvasContext.lineWidth = 2;
+    }
+
+    // Draw the tile map.
+    const colCount = level.colsPerRow;
+    const rowCount = level.tiles.length / colCount;
+
+    for (let y = 0; y < rowCount; y++) {
+      for (let x = 0; x < colCount; x++) {
+        const tile = level.tiles.at(colCount * y + x);
+        this.canvasContext.fillStyle =
+          tile === TILE_WALL ? WALL_COLOR : FLOOR_COLOR;
+
+        this.canvasContext.fillRect(
+          x * tileWidth,
+          y * tileHeight,
+          tileWidth,
+          tileHeight,
+        );
+      }
+    }
+
+    if (drawTileBorders) {
+      for (let y = 0; y < rowCount; y++) {
+        for (let x = 0; x < colCount; x++) {
+          this.canvasContext.strokeRect(
+            x * tileWidth,
+            y * tileHeight,
+            tileWidth,
+            tileHeight,
+          );
+        }
+      }
+    }
+
+    // Draw sprites.
+    const spriteWidth = 48;
+    const spriteHeight = 48;
+    const spriteOffsetX = (tileWidth - spriteWidth) / 2;
+    const spriteOffsetY = (tileHeight - spriteHeight) / 2;
+
+    const goal_count = level.goals.length;
+
+    for (let i = 0; i < goal_count; i++) {
+      const goalX = level.goals[i][0];
+      const goalY = level.goals[i][1];
+
+      this.canvasContext.fillStyle = GOAL_EMPTY_COLOR;
+      this.canvasContext.fillRect(
+        goalX * tileWidth + spriteOffsetX,
+        goalY * tileHeight + spriteOffsetY,
+        spriteWidth,
+        spriteHeight,
+      );
+    }
+
+    const box_count = level.boxes.length;
+
+    for (let i = 0; i < box_count; i++) {
+      const boxX = level.boxes[i][0];
+      const boxY = level.boxes[i][1];
+
+      this.canvasContext.fillStyle = BOX_COLOR;
+      this.canvasContext.fillRect(
+        boxX * tileWidth + spriteOffsetX,
+        boxY * tileHeight + spriteOffsetY,
+        spriteWidth,
+        spriteHeight,
+      );
+    }
+
+    const playerX = level.playerSpawn[0];
+    const playerY = level.playerSpawn[1];
+
+    this.canvasContext.fillStyle = PLAYER_COLOR;
+    this.canvasContext.fillRect(
+      playerX * tileWidth + spriteOffsetX,
+      playerY * tileHeight + spriteOffsetY,
+      spriteWidth,
+      spriteHeight,
+    );
+
+    /*
     this.canvasContext.fillStyle = "pink";
     this.canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
     this.canvasContext.fillStyle = "blue";
     this.canvasContext.fillRect(this.x, this.y, 10, 10);
+    */
   }
 }
---
+++ src/demos/brainfreeze/src/levels/level1.js
@@ -0,0 +1,16 @@
+const level = {
+  // prettier-ignore
+  tiles: [
+    1, 1, 1, 1, 1,
+    1, 0, 0, 0, 1,
+    1, 0, 0, 0, 1,
+    1, 0, 0, 0, 1,
+    1, 1, 1, 1, 1,
+  ],
+  colsPerRow: 5,
+  playerSpawn: [1, 2],
+  boxes: [[2, 2]],
+  goals: [[3, 2]],
+};
+
+export default level;
